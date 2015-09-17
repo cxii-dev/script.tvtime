97dda8318c8c4a1c2a143690822a05533fe571e8
CheiFE <cheife@gmail.com>
2015-09-18 00:27:13 +0300

    Create program.py

--- program.py
+++ program.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+# -*- coding: unicode -*-
 
 import os
 import re
@@ -194,12 +194,12 @@ def log(msg):
 def encode(string):
     result = ''
     try:
-        result = string.encode('UTF-8','replace')
+        result = string.encode('Unicode','replace')
     except UnicodeDecodeError:
         result = 'Unicode Error'
     return result         
 
 def normalizeString(str):
-    return unicodedata.normalize('NFKD', str).encode('ascii','ignore').encode('UTF-8','replace')
+    return unicodedata.normalize('NFKD', str).encode('ascii','ignore').encode('Unicode','replace')
 
 start()
